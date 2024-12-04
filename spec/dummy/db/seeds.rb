a6061690f2b087abce17657663f4257bdfbbc586
Weston Ganger <weston@westonganger.com>
2024-12-04 01:07:01 -0800

    Improve performance significantly using SQL GROUP_BY and pagination of 1000 per page, app tested using seed of 100,000 records (#8)

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,20 +1,30 @@
-(1.month.ago.to_date..Date.today).each do |date|
-  3.times.each do
-    TrackedRequestsByDaySite.create!(
-      day: date,
-      url_hostname: ["example.com", "other-site.com"].sample,
-      platform: SecureRandom.hex(3),
-      browser_engine: SecureRandom.hex(3),
-      total: rand(100)
-    )
+date = Date.today
 
-    TrackedRequestsByDayPage.create!(
-      day: date,
-      url_hostname: ["example.com", "other-site.com"].sample,
-      url_path: ["/", "/foo", "/bar", "/baz"].sample,
-      referrer_hostname: ["example.com", "other-site.com"].sample,
-      referrer_path:  ["/", "/foo", "/bar", "/baz"].sample,
-      total: rand(100)
-    )
-  end
+site = []
+page = []
+
+hostname_options = ["example.com", "other-site.com", "some-product.example.com"]
+platform_options = ["macOS", "Generic Linux", "Windows", "Android", "iOS (iPhone)", "iOS (iPad)"]
+browser_engine_options = ["blink", "gecko", "webkit"]
+
+100_000.times.each do
+  site << {
+    day: date,
+    url_hostname: hostname_options.sample,
+    platform: platform_options.sample,
+    browser_engine: browser_engine_options.sample,
+    total: rand(1000),
+  }
+
+  page << {
+    day: date,
+    url_hostname: hostname_options.sample,
+    url_path: "/posts/#{SecureRandom.hex(3)}/",
+    referrer_hostname: hostname_options.sample,
+    referrer_path: "/posts/#{SecureRandom.hex(3)}/",
+    total: rand(1000),
+  }
 end
+
+TrackedRequestsByDaySite.insert_all(site)
+TrackedRequestsByDayPage.insert_all(page)
